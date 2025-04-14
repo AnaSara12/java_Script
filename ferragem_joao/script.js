5242a28644a34ee3f1e781de4c87de7a583b09fb
ana! <ana.s.nascimento@aluno.senai.br>
2025-04-14 11:12:50 -0500

    modificação de uma loja online

---
+++ ferragem_joao/script.js
@@ -0,0 +1,14 @@
+//MARTELO,PREGO,ALIGATE,PARAFUSO
+//19,90  10,00  23,00  a = 10
+
+const precos={
+    "parafuso":19.90,
+    "martelo":23.70,
+    "chave de fenda":10.99,
+    "serrote":30.00,
+};
+//atualizar campo de preço automaticamente ao escolher produto
+document.getElementById('produto').addEventListener('change',function(){
+    const precos =
+    precos
+})
